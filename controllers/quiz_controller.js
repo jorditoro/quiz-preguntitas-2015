64aee96df9febb98f443de740ab859437dd19750
jorditoro <jordi.tm87@gmail.com>
2015-08-11 18:12:48 +0200

    Ejercicio Modulo 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,12 +14,21 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(
+  if(typeof(req.query.search) != 'undefined'){
+    var busqueda=(req.query.search||"").replace(" ","%");
+     models.Quiz.findAll({where:['pregunta like ?','%'+busqueda+'%'],order:'pregunta ASC'}).then(function(quizes){
+             res.render('quizes/index.ejs',{quizes:quizes,errors:[]});
+     }).catch(function(error) { next(error);}
+    )
+  }
+  else {
+     models.Quiz.findAll().then(
     function(quizes) {
       res.render('quizes/index.ejs', {quizes: quizes, errors: []});
     }
-  ).catch(function(error){next(error)});
-};
+  ).catch(function(error) { next(error);})
+    };
+  }
 
 // GET /quizes/:id
 exports.show = function(req, res) {
